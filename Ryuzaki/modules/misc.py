40d780d7563132e6f1cb350a15509f3039dff298
DopeBotz <87130137+DopeBotz@users.noreply.github.com>
2021-07-09 22:35:38 +0530

    Update misc.py

--- Ryuzaki/modules/misc.py
+++ Ryuzaki/modules/misc.py
@@ -85,41 +85,16 @@ __help__ = """
 *Available commands:*
 *Markdown:*
  • /markdownhelp*:* quick summary of how markdown works in telegram - can only be called in private chats
-*Paste:*
- • /paste*:* Saves replied content to `nekobin.com` and replies with a url
+*Languages:*
+ • /json *:* get message details in json form data
 *React:*
  • /react*:* Reacts with a random reaction 
 *Urban Dictonary:*
  • /ud <word>*:* Type the word or expression you want to search use
 *Wikipedia:*
  • /wiki <query>*:* wikipedia your query
-*Wallpapers:*
- • /wall <query>*:* get a wallpaper from wall.alphacoders.com
 *Currency converter:* 
  • /cash*:* currency converter
-Example:
- `/cash 1 USD INR`  
-      _OR_
- `/cash 1 usd inr`
-Output: `1.0 USD = 75.505 INR`
-
-*MATHS*
-Solves complex math problems using https://newton.now.sh
-• /math*:* Math `/math 2^2+2(2)`
-• /factor*:* Factor `/factor x^2 + 2x`
-• /derive*:* Derive `/derive x^2+2x`
-• /integrate*:* Integrate `/integrate x^2+2x`
-• /zeroes*:* Find 0's `/zeroes x^2+2x`
-• /tangent*:* Find Tangent `/tangent 2lx^3`
-• /area*:* Area Under Curve `/area 2:4lx^3`
-• /cos*:* Cosine `/cos pi`
-• /sin*:* Sine `/sin 0`
-• /tan*:* Tangent `/tan 0`
-• /arccos*:* Inverse Cosine `/arccos 1`
-• /arcsin*:* Inverse Sine `/arcsin 0`
-• /arctan*:* Inverse Tangent `/arctan 0`
-• /abs*:* Absolute Value `/abs -1`
-• /log*:* Logarithm `/log 2l8`
 """
 
 ECHO_HANDLER = DisableAbleCommandHandler("echo", echo, filters=Filters.group)
