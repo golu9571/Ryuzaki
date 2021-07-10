e2fa34c17a760d82a1e18fc6f74458cb251e4ba2
DopeBotz <87130137+DopeBotz@users.noreply.github.com>
2021-07-10 15:09:42 +0530

    Create about_me.py

---
+++ Ryuzaki/modules/about_me.py
@@ -0,0 +1,7 @@
+from telethon import events, Button
+from Ryuzaki import telethn
+
+bnt =[
+    [Button.inline("T&C", data="tc"), Button.inline("Contributors", data="cb")],
+    [Button.inline("Dev", data="dev"), Button.inline("About Me", data="ab")],
+    [Button.inline("Back", data="RyuzakiBot_back")]]
