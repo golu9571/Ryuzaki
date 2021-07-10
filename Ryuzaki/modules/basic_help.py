3a0a6de40e41e6a01c68dfaba3f7b38b957199b3
DopeBotz <87130137+DopeBotz@users.noreply.github.com>
2021-07-10 10:10:22 +0530

    Create basic_help.py

---
+++ Ryuzaki/modules/basic_help.py
@@ -0,0 +1,25 @@
+import io
+from Ryuzaki.events import register
+from Ryuzaki import telethn
+from Ryuzaki import telethn
+from telethon import types
+from telethon import events
+from telethon.tl import functions, types
+from telethon.tl.types import *
+
+HELP_TEXT = """
+**Here's basic Help regarding How to use Me?**
+
+• Firstly Add me to your group by pressing [here](http://t.me/Ryuzakibot?startgroup=true)
+
+• After adding promote me manually with full rights for faster experience.
+
+• Than send `/admincache@TheRyuzakiBot` in that chat to refresh admin list in My database.
+
+*All done now use below given button's to know about use.*
+"""
+
+@telethn.on(events.callbackquery.CallbackQuery(data="basic_help"))
+async def basic_help(event):
+
+    await event.edit(HELP_TEXT, buttons=[[Button.inline("Admin Help", data="admin_help")]])
