2374ef878324503a6f909fa3ca847c94604c056a
DopeBotz <87130137+DopeBotz@users.noreply.github.com>
2021-07-10 10:14:07 +0530

    Update basic_help.py

--- Ryuzaki/modules/basic_help.py
+++ Ryuzaki/modules/basic_help.py
@@ -3,7 +3,7 @@ from Ryuzaki.events import register
 from Ryuzaki import telethn
 from Ryuzaki import telethn
 from telethon import types
-from telethon import events
+from telethon import events, Button
 from telethon.tl import functions, types
 from telethon.tl.types import *
 
@@ -20,6 +20,6 @@ HELP_TEXT = """
 """
 
 @telethn.on(events.callbackquery.CallbackQuery(data="basic_help"))
-async def basic_help(event):
+async def _(event):
 
     await event.edit(HELP_TEXT, buttons=[[Button.inline("Admin Help", data="admin_help")]])
