342b188b3863e04b618e692df976ce92124c7773
DopeBotz <87130137+DopeBotz@users.noreply.github.com>
2021-07-10 17:27:01 +0530

    Update userinfo.py

--- Ryuzaki/modules/userinfo.py
+++ Ryuzaki/modules/userinfo.py
@@ -238,8 +238,10 @@ def info(update: Update, context: CallbackContext):
     else:
         return
 
+    rep = message.reply_text("<code>Appraising...</code>", parse_mode=ParseMode.HTML)
+
     text = (
-        f"╒═══「<b>\Extra
+        f"╒═══「<b> Appraisal results:</b> 」\n"
         f"ID: <code>{user.id}</code>\n"
         f"First Name: {html.escape(user.first_name)}"
     )
@@ -440,7 +442,7 @@ def about_bio(update: Update, context: CallbackContext):
 
     if info:
         update.effective_message.reply_text(
-            "*{}*:\n{}".format(user.first_name, escape_markdown()),
+            "*{}*:\n{}".format(user.first_name, escape_markdown(info)),
             parse_mode=ParseMode.MARKDOWN,
             disable_web_page_preview=True,
         )
@@ -519,8 +521,25 @@ __help__ = """
  • `/id`*:* get the current group id. If used by replying to a message, gets that user's id.
  • `/gifid`*:* reply to a gif to me to tell you its file ID.
 
+*Self addded information:*
+ • `/setme <text>`*:* will set your info
+ • `/me`*:* will get your or another user's info.
+Examples:
+ `/setme I am a wolf.`
+ `/me @username(defaults to yours if no user specified)`
+
+*Information others add on you:*
+ • `/bio`*:* will get your or another user's bio. This cannot be set by yourself.
+• `/setbio <text>`*:* while replying, will save another user's bio
+Examples:
+ `/bio @username(defaults to yours if not specified).`
+ `/setbio This user is a wolf` (reply to the user)
+
 *Overall Information about you:*
  • `/info`*:* get information about a user.
+
+*What is that health thingy?*
+ Come and see [HP System explained](https://t.me/OnePunchUpdates/192)
 """
 
 SET_BIO_HANDLER = DisableAbleCommandHandler("setbio", set_about_bio)
