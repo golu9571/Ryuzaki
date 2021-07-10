05e3ccfa7c025f74dfc45b67da8e74dad1c90789
DopeBotz <87130137+DopeBotz@users.noreply.github.com>
2021-07-10 11:24:22 +0530

    Update nightmodebtn.py

--- Ryuzaki/modules/nightmodebtn.py
+++ Ryuzaki/modules/nightmodebtn.py
@@ -1,8 +1,5 @@
 __mod_name__ = "NightMode"
 __help__ = f"""
- • /nightmode on/off
- 
-Set NightMode - /setnightmode timezone : 12:00:00 PM : 07:00:00 AM
-**Note:** Now you can choose your counrty time zone and opening and closing time also.
+**Under Maintenance**
 """
 
